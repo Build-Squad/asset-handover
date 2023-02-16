a64f01accd2cb4ba31a9abcfb8385f317ab71172
Amarildo Grembi <amarildo@buildsquad.net>
2023-02-16 13:14:38 +0000

    adjust idents

--- api/src/routes/assetHandover.js
+++ api/src/routes/assetHandover.js
@@ -3,105 +3,99 @@ import { body, param } from 'express-validator';
 import { validateRequest } from '../middlewares/validateRequest.js';
 
 function initAssetHandoverRouter(AssetHandoverService) {
-    const router = express.Router();
+  const router = express.Router();
 
-    router.post(
-        '/asset-handover/lockUp/updateCreationFees',
-        [
-            body('fees', 'Enter a valid fee in decimal format.')
-                .trim()
-                .isFloat(),
-        ],
-        validateRequest,
-        async (req, res) => {
-            const fees = parseFloat(req.body).toFixed(8);
-            console.log('body: ' + JSON.stringify(fees));
-            const tx = await AssetHandoverService.updateCreationFees(
-                fees
-            );
-            return res.send({
-                transaction: tx,
-            });
-        }
-    );
+  router.post(
+    '/asset-handover/lockUp/updateCreationFees',
+    [
+      body('fees', 'Enter a valid fee in decimal format.')
+        .trim()
+        .isFloat(),
+    ],
+    validateRequest,
+    async (req, res) => {
+      const fees = parseFloat(req.body).toFixed(8);
+      console.log('body: ' + JSON.stringify(fees));
+      const tx = await AssetHandoverService.updateCreationFees(fees);
+      return res.send({
+        transaction: tx,
+      });
+    }
+  );
 
-    router.post(
-        '/asset-handover/lockUp/updateWithdrawFees',
-        [
-            body('fees', 'Enter a valid fee in decimal format.')
-                .trim()
-                .isFloat(),
-        ],
-        validateRequest,
-        async (req, res) => {
-            const fees = parseFloat(req.body).toFixed(8);
-            console.log('body: ' + JSON.stringify(req.body));
-            const tx = await AssetHandoverService.updateWithdrawFees(
-                fees
-            );
-            return res.send({
-                transaction: tx,
-            });
-        }
-    );
+  router.post(
+    '/asset-handover/lockUp/updateWithdrawFees',
+    [
+      body('fees', 'Enter a valid fee in decimal format.')
+        .trim()
+        .isFloat(),
+    ],
+    validateRequest,
+    async (req, res) => {
+      const fees = parseFloat(req.body).toFixed(8);
+      console.log('body: ' + JSON.stringify(req.body));
+      const tx = await AssetHandoverService.updateWithdrawFees(fees);
+      return res.send({
+        transaction: tx,
+      });
+    }
+  );
 
-    router.get(
-        '/asset-handover/accountLockUp/:address',
-        [
-            param('address', 'Enter a valid Flow address.')
-                .trim()
-                .isLength({ min: 18, max: 18 })
-                .isAlphanumeric(),
-        ],
-        validateRequest,
-        async (req, res) => {
-            const accountLockUp =
-                await AssetHandoverService.getAccountLockUp(
-                    req.params.address
-                );
-            return res.status(200).send({ accountLockUp });
-        }
-    );
+  router.get(
+    '/asset-handover/accountLockUp/:address',
+    [
+      param('address', 'Enter a valid Flow address.')
+        .trim()
+        .isLength({ min: 18, max: 18 })
+        .isAlphanumeric(),
+    ],
+    validateRequest,
+    async (req, res) => {
+      const accountLockUp =
+        await AssetHandoverService.getAccountLockUp(
+          req.params.address
+        );
+      return res.status(200).send({ accountLockUp });
+    }
+  );
 
-    router.get(
-        '/asset-handover/fungibleTokenInfoMapping',
-        async (req, res) => {
-            const fungibleTokenInfoMapping =
-                await AssetHandoverService.getFungibleTokenInfoMapping();
-            return res.status(200).send({ fungibleTokenInfoMapping });
-        }
-    );
+  router.get(
+    '/asset-handover/fungibleTokenInfoMapping',
+    async (req, res) => {
+      const fungibleTokenInfoMapping =
+        await AssetHandoverService.getFungibleTokenInfoMapping();
+      return res.status(200).send({ fungibleTokenInfoMapping });
+    }
+  );
 
-    router.get(
-        '/asset-handover/nonFungibleTokenInfoMapping',
-        async (req, res) => {
-            const nonFungibleTokenInfoMapping =
-                await AssetHandoverService.getNonFungibleTokenInfoMapping();
-            return res
-                .status(200)
-                .send({ nonFungibleTokenInfoMapping });
-        }
-    );
+  router.get(
+    '/asset-handover/nonFungibleTokenInfoMapping',
+    async (req, res) => {
+      const nonFungibleTokenInfoMapping =
+        await AssetHandoverService.getNonFungibleTokenInfoMapping();
+      return res.status(200).send({ nonFungibleTokenInfoMapping });
+    }
+  );
 
-    router.get(
-        '/asset-handover/LockUpsByRecipient/:address',
-        [
-            param('address', 'Enter a valid Flow address.')
-                .trim()
-                .isLength({ min: 18, max: 18 })
-                .isAlphanumeric(),
-        ],
-        validateRequest,
-        async (req, res) => {
-            const lockUpsByRecipient =
-                await AssetHandoverService.getLockUpsByRecipient(
-                    req.params.address
-                );
-            return res.status(200).send({ lockUpsByRecipient });
-        }
-    );
+  router.get(
+    '/asset-handover/LockUpsByRecipient/:address',
+    [
+      param('address', 'Enter a valid Flow address.')
+        .trim()
+        .isLength({ min: 18, max: 18 })
+        .isAlphanumeric(),
+    ],
+    validateRequest,
+    async (req, res) => {
+      const lockUpsByRecipient =
+        await AssetHandoverService.getLockUpsByRecipient(
+          req.params.address
+        );
+      return res.status(200).send({ lockUpsByRecipient });
+    }
+  );
 
-    return router;
+  return router;
 }
 
 export default initAssetHandoverRouter;
