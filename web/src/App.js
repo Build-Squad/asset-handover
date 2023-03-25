6412cc1e73f50e48334c9e9c892d96a6e13b7ea5
Olive105 <105.olivesoft@gmail.com>
2023-03-26 01:07:56 +0900

    feat - add Home widget

--- web/src/App.js
+++ web/src/App.js
@@ -3,12 +3,16 @@ import {BrowserRouter as Router, Routes, Route} from "react-router-dom";
 import './App.css';
 import Header from "./layout/Header";
 import Footer from "./layout/Footer";
+import Home from "./pages/Home";
 
 function App() {
   return (
     <div className="container pt-3">
       <Router>
         <Header />
+        <Routes>
+          <Route path="" element={<Home />} />
+        </Routes>
         <Footer />
       </Router>
     </div>
---
+++ web/src/pages/Home.js
@@ -0,0 +1,7 @@
+export default function Home(){
+  return(
+    <div className="coming-pad">
+      
+    </div>
+  )
+}
