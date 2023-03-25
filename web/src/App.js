029c1ceeb949ae6dfe87b87bc313de04a356cc18
Olive105 <105.olivesoft@gmail.com>
2023-03-26 01:00:48 +0900

    feat - implement Header and Footer

--- web/src/App.js
+++ web/src/App.js
@@ -1,25 +1,16 @@
 import {BrowserRouter as Router, Routes, Route} from "react-router-dom";
 
-import logo from './logo.svg';
 import './App.css';
+import Header from "./layout/Header";
+import Footer from "./layout/Footer";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="container pt-3">
+      <Router>
+        <Header />
+        <Footer />
+      </Router>
     </div>
   );
 }
