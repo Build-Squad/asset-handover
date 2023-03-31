a07bae81d6861d5f959519732c5904537c9d72fe
Olive105 <105.olivesoft@gmail.com>
2023-03-31 22:26:30 +0900

    feat - implement Pledges item page

--- web/src/pages/Backup.js
+++ web/src/pages/Backup.js
@@ -1,17 +1,19 @@
 import { useEffect, useState } from 'react';
 import * as fcl from "@onflow/fcl";
-import { Tab, Nav, Card, Button, Form, } from "react-bootstrap";
+import { Tab, Nav, Card, Button, Form, Badge } from "react-bootstrap";
 import { useNavigate } from "react-router-dom";
 import { FaPlus } from 'react-icons/fa';
 
 export default function Backup() {
   const [user, setUser] = useState({ loggedIn: null });
   const [step, setStep] = useState("default");
+  const [pledgeStep, setPledgeStep] = useState("default");
   const navigate = useNavigate();
 
   useEffect(() => { 
     fcl.currentUser.subscribe(setUser);
     setStep("nfts");
+    setPledgeStep("item");
   }, []);  
 
   const logout = () => {
@@ -41,7 +43,7 @@ export default function Backup() {
             <Nav.Item className="type" onClick={() => logout()}>
               <Nav.Link eventKey="third" className="text-center">
                 <p className='text-grey mb-0'>
-                  {user.addr} 4354354645
+                  {user.addr}
                 </p>                
                 <img className='mt-1' src="wallet1.png" width="50%" height="50%" />
                 <h5 className="mt-3 blue-font">DISCONNECT <br/> WALLET</h5>
@@ -735,11 +737,183 @@ export default function Backup() {
             </Tab.Pane>
             }
             
+            {pledgeStep === "default" &&
             <Tab.Pane eventKey="second">
-              <h1 className="text-white">
-                Pledges Content
-              </h1>
+              <div className='row'>
+                <div className='col-xl-3 col-lg-5'>
+                  <Card className="text-center cursor-pointer" onClick={() => setPledgeStep("item")}>
+                    <Card.Img className='item-img' variant="top" src="pleages.png" />
+                    <Card.Body className='p-0'>
+                      <Card.Title className="blue-font">Lorem ipsum dolor</Card.Title>
+                      <p className='text-grey mb-0'>
+                        {user.addr}
+                      </p>
+                      <p className='font-14 mb-0 blue-font'>Created on</p>
+                      <p className='mb-1 blue-font'>12 March 2023</p>
+
+                      <p className='red-font font-14 mb-0'>Maturity Date</p>
+                      <p className='red-font'>3 Jan 2027</p>
+                    </Card.Body>
+                  </Card>
+                </div>
+
+                <div className='col-xl-3 col-lg-5'>
+                  <Card className="text-center">
+                    <Card.Img className='item-img' variant="top" src="pleages.png" />
+                    <Card.Body className='p-0'>
+                      <Card.Title className="blue-font">Lorem ipsum dolor</Card.Title>
+                      <p className='text-grey mb-0'>
+                        {user.addr}
+                      </p>
+                      <p className='font-14 mb-0 blue-font'>Created on</p>
+                      <p className='mb-1 blue-font'>12 March 2023</p>
+
+                      <p className='red-font font-14 mb-0'>Maturity Date</p>
+                      <p className='red-font'>3 Jan 2027</p>
+                    </Card.Body>
+                  </Card>
+                </div>
+
+                <div className='col-xl-3 col-lg-5'>
+                  <Card className="text-center">
+                    <Card.Img className='item-img' variant="top" src="pleages.png" />
+                    <Card.Body className='p-0'>
+                      <Card.Title className="blue-font">Lorem ipsum dolor</Card.Title>
+                      <p className='text-grey mb-0'>
+                        {user.addr}
+                      </p>
+                      <p className='font-14 mb-0 blue-font'>Created on</p>
+                      <p className='mb-1 blue-font'>12 March 2023</p>
+
+                      <p className='red-font font-14 mb-0'>Maturity Date</p>
+                      <p className='red-font'>3 Jan 2027</p>
+                    </Card.Body>
+                  </Card>
+                </div>
+
+                <div className='col-xl-3 col-lg-5'>
+                  <Card className="text-center">
+                    <Card.Img className='item-img' variant="top" src="pleages.png" />
+                    <Card.Body className='p-0'>
+                      <Card.Title className="blue-font">Lorem ipsum dolor</Card.Title>
+                      <p className='text-grey mb-0'>
+                        {user.addr}
+                      </p>
+                      <p className='font-14 mb-0 blue-font'>Created on</p>
+                      <p className='mb-1 blue-font'>12 March 2023</p>
+
+                      <p className='red-font font-14 mb-0'>Maturity Date</p>
+                      <p className='red-font'>3 Jan 2027</p>
+                    </Card.Body>
+                  </Card>
+                </div>
+              </div>
             </Tab.Pane>
+            }
+
+            {pledgeStep === "item" &&
+            <Tab.Pane eventKey="second">
+              <div className='row p-3 mb-3'>
+                <div className='col-md-6'>
+                  <div className='row'>
+                    <div className='col-md-3 d-flex green-border'>
+                      <img src="pleages.png" width="100%" height="auto" />
+                    </div>
+
+                    <div className='col-md-9'>
+                      <h5 className='blue-font'>Lorem ipsum dolor</h5>
+                      <p className='blue-font mb-0'>Lorem ipsum dolor Lorem ipsum dolor</p>
+                      <p className='blue-font mb-1'>Lorem ipsum dolor Lorem ipsum dolor</p>
+                      <p className='text-grey'>{user.addr}</p>
+                    </div>
+                  </div>
+                </div>
+
+                <div className='col-md-6 text-webkit-right'>
+                  <p className='font-bold maturity-date blue-bg border-none'>
+                    MATURITY DATE: 12-08-2028
+                  </p>                  
+                </div>
+              </div>
+
+              <h4 className='p-2 border-bottom-green blue-font'>
+                COIN(S)
+                <Badge className='mx-3' bg="danger">WITHDRAW</Badge>
+              </h4>
+              <div className='row'>
+                <div className='col-md-1'>
+                  <img src="flowcoin.png" width="100%" height="auto" />
+                  <p className='blue-font font-bold text-center'>(250)</p>
+                </div>
+
+                <div className='col-md-1'>
+                  <img src="coin.png" width="100%" height="auto" />
+                  <p className='blue-font font-bold text-center'>(250)</p>
+                </div>
+
+                <div className='col-md-1'>
+                  <img src="flowcoin.png" width="100%" height="auto" />
+                  <p className='blue-font font-bold text-center'>(100)</p>
+                </div>
+
+                <div className='col-md-1'>
+                  <img src="coin.png" width="100%" height="auto" />
+                  <p className='blue-font font-bold text-center'>(150)</p>
+                </div>
+
+                <div className='col-md-1'>
+                  <img src="flowcoin.png" width="100%" height="auto" />
+                  <p className='blue-font font-bold text-center'>(250)</p>
+                </div>
+
+                <div className='col-md-1'>
+                  <img src="coin.png" width="100%" height="auto" />
+                  <p className='blue-font font-bold text-center'>(250)</p>
+                </div>
+
+                <div className='col-md-1'>
+                  <img src="flowcoin.png" width="100%" height="auto" />
+                  <p className='blue-font font-bold text-center'>(250)</p>
+                </div>
+
+                <div className='col-md-1'>
+                  <img src="coin.png" width="100%" height="auto" />
+                  <p className='blue-font font-bold text-center'>(250)</p>
+                </div>
+              </div> 
+
+              <h4 className='p-2 border-bottom-green blue-font'>
+                NFT COLLECTION(S)
+                <Badge className='mx-3' bg="danger">WITHDRAW</Badge>
+              </h4>
+              <div className='row'>
+                <div className='col-md-3 pt-2'>
+                  <Card className='p-2 pb-1 cursor-pointer' onClick={() => setStep("nfts")}>
+                    <Card.Img variant="top" src="nfts.png" />
+                    <Card.Body className='pb-0'>
+                      <div className='row'>
+                        <div className='col-3 p-0'>
+                          <img className='nft-img' src="nft.png" width="100%" height="auto" />
+                          <h5 className='text-center'>(7)</h5>
+                        </div>
+
+                        <div className='col-9'>
+                          <p className='font-bold font-15 mb-0'>Lorem ipsum dolor</p>
+                          <div className='d-flex'>
+                            <p className='text-grey font-12 mb-0'>
+                              Lorem ipsum dolor Lorem <br/>
+                              Lorem ipsum dolor Lorem
+                            </p>
+                          </div>                          
+                        </div>
+                      </div>                      
+                    </Card.Body>
+                  </Card>
+                </div>
+              </div>
+            </Tab.Pane>
+            }
+            
           </Tab.Content>
         </div>        
       </div>
