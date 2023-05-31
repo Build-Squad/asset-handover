43e79cc1f3b798888ee72e03be8f7592bf79c936
EndlessLucky <105.olivesoft@gmail.com>
2023-05-31 09:05:59 +0900

    fix - validFlowAddress

--- web/src/utils/utils.js
+++ web/src/utils/utils.js
@@ -1,7 +1,12 @@
+import { Buffer } from 'buffer';
+
+
 export const isValidFlowAddress = (address) => {
     if (!address.startsWith("0x") || address.length !== 18) {
         return false
     }
+    const bytes = Buffer.from(address.replace("0x", ""), "hex")
+    if (bytes.length !== 8) { return false }
     return true
 }
 
